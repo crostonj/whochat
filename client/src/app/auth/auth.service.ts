fca89afd3ae00489d21ae903e9218fdc8e53c0be
crostonj <crostonj@gmail.com>
2018-06-08 09:45:18 -0600

    Got client working.

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -4,15 +4,17 @@ import { Injectable } from "@angular/core";
 //import 'rxjs/Rx'
 import { map } from "rxjs/operators";
 import { Observable } from 'rxjs';
+import { Subject } from "rxjs";
 import { Http, RequestOptions, Response, Headers } from "@angular/http";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class AuthService {
 
     public DATA_HOST = 'http://localhost:5000/';
     private token: string;
     private isAuthenticated: boolean;
     private userSvcUrl = this.DATA_HOST + 'user/';
+    private authStatusListener = new Subject<boolean>();
 
     constructor(private http: Http){
         
@@ -37,6 +39,10 @@ export class AuthService {
                             });
      }
 
+     getAuthStatusListener() {
+        return this.authStatusListener.asObservable();
+      }
+
      logout(){
          localStorage.clear();
      }
@@ -50,7 +56,8 @@ export class AuthService {
      }
 
      getIsAuth() {
-        return this.isAuthenticated;
+         return true;
+        //return this.isAuthenticated;
       }
     
 }
